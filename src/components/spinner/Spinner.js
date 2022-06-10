8bc030f7c9494a2bad611af9fb271dac9add7354
Pedro Perez <pedrodrums2015@gmail.com>
2022-06-10 14:27:29 -0400

    Edit Spinner.js

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Loader, Img } from "./SpinnerStyle";
-import loading from "../../img/loading.gif";
+import loading from "../../img/spinner.gif";
 
 export const Spinner = () => {
   return (
